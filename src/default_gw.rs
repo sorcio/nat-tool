1402db553719bafd47da76f45b404fb2daadb734
Davide Rizzo <sorcio@gmail.com>
2024-06-25 13:04:31 +0200

    move instead of clone

--- src/default_gw.rs
+++ src/default_gw.rs
@@ -14,10 +14,10 @@ pub(crate) enum DefaultGatewayError {
 pub(crate) fn get_default_gateway() -> Result<Ipv4Addr, DefaultGatewayError> {
     let dev =
         netdev::get_default_gateway().map_err(DefaultGatewayError::CannotFindDefaultGateway)?;
-    if let Some(ipv4) = dev.ipv4.first() {
-        Ok(ipv4.clone())
-    } else if let Some(ipv6) = dev.ipv6.first() {
-        Err(DefaultGatewayError::Ipv6Unsupported(ipv6.clone()))
+    if let [ipv4, ..] = dev.ipv4[..] {
+        Ok(ipv4)
+    } else if let [ipv6, ..] = dev.ipv6[..] {
+        Err(DefaultGatewayError::Ipv6Unsupported(ipv6))
     } else {
         Err(DefaultGatewayError::CannotFindDefaultGateway(
             "no valid address".to_string(),
